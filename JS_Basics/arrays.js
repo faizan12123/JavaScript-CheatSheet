9198e4fa8c2875270d395967efa8c0d6d948a752
Faizan <faizan121234@gmail.com>
2022-08-05 02:13:51 -0700

    updated file structure and arrays.js

--- JS_Basics/arrays.js
+++ JS_Basics/arrays.js
@@ -1,20 +1,44 @@
 //Arrays- variables that hold multiple values
 
+// create an array
 const fruits = ['apples', 'oranges', 'pears', 10, true]; // JS allows multiple data types in a single array
+const cars = new Array("Saab", "Volvo", "BMW");
+const points = new Array(40); // Create an array with 40 undefined elements
+const points2 = [40, 100, 1, 5, 25, 10];
+console.log(fruits)
+console.log(cars)
 
+// add to an array
 fruits[5] = 'grapes'; //adds 6th item at index 5 that is 'grapes'
-
-fruits.push('mangos'); //adds 'mangos' to the end of the array
+fruits.push('mangos'); //adds to the end of the array
 fruits.unshift('Strawberries'); //adds 'Strawberries' to the front of the array
+console.log(fruits.concat("peaches")) //adds to end of array
+fruits.splice(2, 0, "Lemon", "Kiwi"); // The first parameter (2) defines the position where new elements should be added (spliced in). The second parameter (0) defines how many elements should be removed. The rest of the parameters ("Lemon" , "Kiwi") define the new elements to be added.
+console.log(fruits)
+
+// Changing an array element
+cars[0] = "Opel";
+console.log(cars)
+
+// delete from array
 fruits.pop(); //takes off last item in array
+fruits.shift(); // delete first item
+fruits.splice(0, 1); // The first parameter (0) defines the position where new elements should be added (spliced in). The second parameter (1) defines how many elements should be removed.
+const citrus = fruits.slice(2, 5); // The slice() method creates a new array. The slice() method does not remove any elements from the source array. The method then selects elements from the start argument, and up to (but not including) the end argument.
+console.log("Fruits array after deleting: " + fruits)
+console.log("Citrus: " + citrus)
 
-console.log(fruits.indexOf('oranges')); //gets the index of a string or any item inside of the array
+// Get index
+console.log("index of oranges is: " + fruits.indexOf('oranges')); 
 
-console.log(fruits[1]); //prints the index 1 (oranges)
-console.log(fruits);
+// Access data from array  (In JavaScript, arrays use numbered indexes and objects use named indexes. If you use named indexes, JavaScript will redefine the array to an object.)
+console.log(fruits[2]); //prints the index 2
+
+// Length of an arrays
+console.log("Length of fruits array is " + fruits.length)
 
 
-
+// Array of objects
 const todos = [
     {
         id:1, 
@@ -33,13 +57,15 @@ const todos = [
     }
 ];
 
+// Access data from array of objects
 console.log(todos[1].text); //prints out the text from the second object in the array; "meeting with boss"
 
-const todoJson = JSON.stringify(todos); //turns todos array into json format
+// Turn array into JSON format
+const todoJson = JSON.stringify(todos);
 console.log(todoJson);
 
 
-//map- to create a new array based off of an array
+//map (to create a new array based off of an array)
 const todoText = todos.map(function(todo){
     return todo.text; //returns an array with all of the text values within todo array
 });
@@ -52,10 +78,40 @@ const todoCompleted = todos.filter(function(todo){
 console.log(todoCompleted);
 
 //map and filter combined
-//filter
 const todoCompletedAndText = todos.filter(function(todo){
     return todo.isCompleted == true; //return the todos that are completed (iscompleted = true)
 }).map(function(todo){
     return todo.text;
 });
 console.log(todoCompletedAndText);
+
+//convert array into string
+console.log(fruits.toString())
+console.log(fruits.join(" * ")) // lets u specify the separator
+
+// Merging arrays
+console.log(fruits.concat(cars)) // concat() method can take any number of array arguments
+
+// Sorting an array alphabetically
+console.log("Sorted fruits: " + fruits.sort())
+
+// Reversing an array 
+console.log("Reversed fruits: " + fruits.reverse())
+
+// Numeric Sort ascending
+console.log(points2.sort(function(a, b){return a - b}))
+
+// Numeric Sort descending
+console.log(points2.sort(function(a, b){return b - a}))
+
+// Sorting an Array in Random Order
+console.log(points2.sort(function(a, b){return 0.5 - Math.random()})); //
+
+// Find highest number in an array
+console.log(Math.max.apply(null, points2))
+
+// Find the lowest number in an array
+console.log(Math.min.apply(null, points2))
+
+// Reduce (reduce() method runs a function on each array element to produce (reduce it to) a single value. The reduce() method does not reduce the original array.)
+console.log( points2.reduce((total, value) => total + value))
--- JS_Basics/variables.js
+++ JS_Basics/variables.js
@@ -2,12 +2,6 @@
 //const is better to use than let if u are not going to change the values of the variable throughout the code.
 
 
-
-
-
-
-
-
 // data types: String, Numbers, Boolean, Null, undefined. Examples of these data types:
 const name = 'John'; //string
 const age = 30; //number
@@ -16,4 +10,12 @@ const isCool = true; //boolean
 const x = null; //null
 const y =undefined; //undefined
 let z; //undefined
+
+//check type
+console.log(typeof age)
+console.log(typeof rating)
+console.log(typeof isCool)
+console.log(typeof x)
+console.log(typeof z)
+
  
